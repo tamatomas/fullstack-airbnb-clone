284c0716fc6fbce87807efa625c42b77613a4ce4
tamatomas <tomastamagnone.lamarti@gmail.com>
2020-12-16 07:55:51 -0300

    price input type

--- packages/web/src/pages/become-a-host/description.tsx
+++ packages/web/src/pages/become-a-host/description.tsx
@@ -79,6 +79,7 @@ export function Description(props: Props) {
                 error={!!errors.price}
                 errmsg={errors.price?.message}
                 value={value}
+                type={"number"}
                 onBlur={onBlur}
                 onChange={onChange}
               />
